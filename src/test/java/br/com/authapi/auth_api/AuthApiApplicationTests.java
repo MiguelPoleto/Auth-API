e81312fe6ce1406a71e047f074ae5f2a80d88745
Miguel <miguelpoleto5@gmail.com>
2025-02-25 14:18:40 -0300

    Initial commit

---
+++ src/test/java/br/com/authapi/auth_api/AuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.authapi.auth_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
